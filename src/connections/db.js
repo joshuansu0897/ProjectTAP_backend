24482b5af19187ee46899e402a9f845faddda793
Joshua Saucedo <joshuansu0897@gmail.com>
2018-11-25 13:52:03 -0700

    add db connection

---
+++ src/connections/db.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+
+const name = process.env.NAME_DB || 'ExampleDB'
+const user = process.env.USER_DB || 'root'
+const pass = process.env.PASS_DB || 'example'
+const host = process.env.HOST_DB || 'localhost'
+const port = process.env.PORT_DB || 3306
+
+const sequelize = new Sequelize(name, user, pass, {
+  host: host,
+  dialect: 'mysql',
+  port: port,
+  logging: console.log,
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
+})
+
+module.exports = sequelize
